b7f32ff7765707904785d1900812ed4a9d86dec5
Carrington-dev <crn96m@gmail.com>
2025-02-13 10:22:21 +0200

    Added Post-List Test Successful

--- news/urls.py
+++ news/urls.py
@@ -7,5 +7,5 @@ router = DefaultRouter()
 router.register("posts", PostViewSet, basename="post")
 
 urlpatterns = [
-    path("api/v1/", include(router.urls))
+    path("", include(router.urls))
 ]
--- stemweb/urls.py
+++ stemweb/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from stemweb.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", view=home, name="home")
+    path("", view=home, name="home"),
+    path("api/v1/", include("news.urls")),
 ]
