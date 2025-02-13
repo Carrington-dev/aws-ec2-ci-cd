a326a9f1d486f2de4b748e51bc16577948804065
Carrington-dev <crn96m@gmail.com>
2025-02-13 10:16:02 +0200

    Added PostViewSet

--- news/views.py
+++ news/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from news.models import Post
+from news.serializers import PostSerializer
+
+class PostViewSet(ModelViewSet):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+    model = Post
