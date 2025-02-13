46598db6c80ef6ce5ef6ef2b285a51f240355288
Carrington-dev <crn96m@gmail.com>
2025-02-13 09:54:48 +0200

    Added Post Model

--- news/models.py
+++ news/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+length  = 256
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=length,)
+    author = models.CharField(max_length=length,)
+    content = models.TextField()
+    date_updated = models.DateTimeField(auto_now=True)
+    date_posted = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title}"
+
+    def __unicode__(self):
+        return f"{self.title}"
